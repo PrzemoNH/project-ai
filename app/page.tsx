6cd3ade372c55ed0a11c2e3a4c075ba62cb7246c
PrzemoNH <przemekzadlo@gmail.com>
2026-07-11 12:16:05 +0200

    Create page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+import header from "@/components/header";
+import hero from "@/components/hero";
+import footer from "@/components/footer";
+
+export default function page() {
+  return (
+    <>
+      <header />
+      <main>
+        <hero />
+      </main>
+      <footer />
+    </>
+  );
+}
