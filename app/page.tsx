6cdfc3db85dfb2748e90fb78d4999a9b3b68902b
PrzemoNH <przemekzadlo@gmail.com>
2026-07-11 20:42:47 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import Header from "@/components/header";
 import Hero from "@/components/hero";
+import Features from "@/components/features";
 import Footer from "@/components/footer";
 
 export default function Page() {
@@ -8,6 +9,7 @@ export default function Page() {
       <Header />
       <main>
         <Hero />
+        <Features />
       </main>
       <Footer />
     </>
