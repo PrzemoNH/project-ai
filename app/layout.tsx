bca5972c7c86e1846dcb7d52c4939a96608af843
PrzemoNH <przemekzadlo@gmail.com>
2026-07-11 12:15:17 +0200

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Project-AI",
+  description:
+    "Twórz strony internetowe, aplikacje i gry z pomocą sztucznej inteligencji.",
+};
+
+export default function root_layout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="pl">
+      <body>{children}</body>
+    </html>
+  );
+}
