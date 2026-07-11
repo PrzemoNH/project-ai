21abbb05a52379c127880a1d22ce64acc72fd22e
PrzemoNH <przemekzadlo@gmail.com>
2026-07-11 16:47:54 +0200

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -2,17 +2,18 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header>
-      <div style={{ textAlign:"center"}}>
+    <header className="header">
+      <div className="logo">
         <Image
           src="/images/logo/project-ai-logo.svg"
           alt="project-ai"
           width={450}
           height={125}
+          priority
         />
       </div>
 
-      <nav>
+      <nav className="nav">
         <a href="#">Kreator</a>
         <a href="#">Studio</a>
         <a href="#">Projekty</a>
