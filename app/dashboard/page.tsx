5e72622aaea94e4faaf7908e851398584c6e2e54
PrzemoNH <przemekzadlo@gmail.com>
2026-07-14 17:16:47 +0200

    Update page.tsx

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,6 +1,7 @@
 import DashboardHeader from "@/components/dashboard/dashboard-header";
 import DashboardCards from "@/components/dashboard/DashboardCards";
 import RecentProjects from "@/components/dashboard/RecentProjects";
+import Image from "next/image";
 
 export default function Dashboard() {
   return (
@@ -9,7 +10,16 @@ export default function Dashboard() {
 
       <main className="dashboard">
 
-        
+        <div className="dashboard-background">
+  <Image
+    src="/images/logo/project-ai-icon-bg.svg"
+    alt=""
+    width={900}
+    height={900}
+    className="dashboard-bg"
+    priority
+  />
+</div>
 
         <h1>
           Witaj w Project-AI 👋
