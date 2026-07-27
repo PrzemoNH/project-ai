51e70e44a2269527b95e6399baab3f947e5dce79
PrzemoNH <przemekzadlo@gmail.com>
2026-07-27 12:05:06 +0200

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-const supabaseUrl = process.env.https://ixyismzenlorsxygwxnv.supabase.co/rest/v1/!;
-const supabaseAnonKey = process.env.eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4eWlzbXplbmxvcnN4eWd3eG52Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODQ5MTY1NzEsImV4cCI6MjEwMDQ5MjU3MX0.BgC2dZffrXnD0NFxQBmLAq-dG3-R7pG4qy0R6olpjTM!;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
