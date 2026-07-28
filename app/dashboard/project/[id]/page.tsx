c0d4c47a9a41fd038e36d5da57a36408c7580494
PrzemoNH <przemekzadlo@gmail.com>
2026-07-28 06:08:45 +0200

    Update page.tsx

--- app/dashboard/project/[id]/page.tsx
+++ app/dashboard/project/[id]/page.tsx
@@ -1,1 +1,21 @@
- 
+ import "../../dashboard.css";
+import AuthGuard from "@/components/dashboard/AuthGuard";
+import DashboardHeader from "@/components/dashboard/dashboard-header";
+import ProjectDetail from "@/components/dashboard/ProjectDetail";
+
+export default async function ProjectPage({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+
+  return (
+    <AuthGuard>
+      <DashboardHeader />
+      <main className="dashboard">
+        <ProjectDetail projectId={id} />
+      </main>
+    </AuthGuard>
+  );
+}
