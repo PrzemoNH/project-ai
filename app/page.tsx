dbf09e2625d83cc7475eae8a6ac4bdd11d6bb70d
PrzemoNH <przemekzadlo@gmail.com>
2026-07-29 07:57:06 +0200

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import Header from "@/components/header";
 import LandingCard from "@/components/LandingCard";
 import Hero from "@/components/hero";
 import Features from "@/components/features";
+import HowItWorks from "@/components/HowItWorks";
 import Footer from "@/components/footer";
 
 export default function Page() {
@@ -12,6 +13,7 @@ export default function Page() {
         <LandingCard />
         <Hero />
         <Features />
+        <HowItWorks />
       </main>
       <Footer />
     </>
